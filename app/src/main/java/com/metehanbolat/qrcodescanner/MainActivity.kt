389fca1387e4aa4b7ad840f8b031b93fe89d9f61
metehanie <metehan.bolat.ie@gmail.com>
2021-12-19 17:49:15 +0300

    QrCodeAnalyzer coded.

--- app/src/main/java/com/metehanbolat/qrcodescanner/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/qrcodescanner/MainActivity.kt
@@ -3,6 +3,10 @@ package com.metehanbolat.qrcodescanner
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
 import com.metehanbolat.qrcodescanner.ui.theme.QRCodeScannerTheme
 
 class MainActivity : ComponentActivity() {
